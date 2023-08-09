80799ebd4254536b267eafc6d8478149b1f4ed2d
Boki <79083667+EX3mKing@users.noreply.github.com>
2023-08-09 17:30:42 +0200

    enemy work
    add enemy tracking
    add enemy attack
    add enemy spawn wait

--- main.lua
+++ main.lua
@@ -1,12 +1,16 @@
-_FL = 0.1 -- frame lenght
-_DS = 4   -- display scale factor
-timers = {}
+_FL = 0.1       -- frame lenght
+_DS = 4         -- display scale factor
+timers = {}     -- timers table
+last_id = 0     -- id of last enemy
 
 function love.load()
-    love.window.setMode(800, 600)
+    love.window.setMode(200 * _DS, 150 * _DS)
     love.window.setTitle( "Damm I LÖVE vampires" )
-    anim8 = require 'libraries/anim8'
     love.graphics.setDefaultFilter("nearest", "nearest")
+    
+    anim8 = require 'libraries/anim8'
+
+    math.randomseed(os.time())
 
     spriteSheet = love.graphics.newImage('sprites/sheet.png')
     grid = anim8.newGrid( 32, 32, spriteSheet:getWidth(), spriteSheet:getHeight())
@@ -21,14 +25,20 @@ function love.load()
     player.animations.idle = anim8.newAnimation( grid('1-4', 2), _FL )
     player.anim = player.animations.idle
 
+    enmy = {}
+
     enemy = {}
+    enemy.id = 0
     enemy.x = 100
     enemy.y = 75
-    enemy.speed = 1
+    enemy.speed = 0.25
     enemy.looking_right = true
+    enemy.attack_distance = 16
     enemy.animations = {}
     enemy.animations.attack = anim8.newAnimation( grid('1-4', 4) , _FL )
     enemy.animations.idle = anim8.newAnimation( grid('1-4', 3), _FL )
+    enemy.animations.spawn_circle = anim8.newAnimation( grid('1-4', 5), _FL)
+    enemy.animations.spawn = anim8.newAnimation( grid('1-4', 6), _FL)
     enemy.anim = enemy.animations.idle
 
 
@@ -36,73 +46,15 @@ function love.load()
     background.sky = love.graphics.newImage('sprites/sky.png')
     background.planet = love.graphics.newImage('sprites/planet.png')
     background.arena = love.graphics.newImage('sprites/arena.png')
-
-    actors = {}
-    actors.foreground = {}
-    actors.background = {}
-
-    -- table.insert(actors.foreground, enemy)
-
-    --player.spriteSheet = love.graphics.newImage('sprites/player.png')
-    --player.grid = anim8.newGrid( 32, 32, player.spriteSheet:getWidth(), player.spriteSheet:getHeight())
-    --player.animations.attack = anim8.newAnimation( player.grid('1-4', 1) , _FL )
-    --player.animations.idle = anim8.newAnimation( player.grid('1-4', 2), _FL )
+    
 end
 
 function love.update(dt)
-
     TickTimers(dt)
-    -- enemy.anim:update(dt)
-
-    UpdateAnimations(actors.foreground, dt)
-
-    if len(timers) > 0 then
-        player.anim:update(dt)
-        return
-    end
-
-    x = 0
-    y = 0
-
-    if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
-        x = x + 1
-    end
-
-    if love.keyboard.isDown("left") or love.keyboard.isDown("a") then
-        x = x - 1
-    end
-
-    if love.keyboard.isDown("down") or love.keyboard.isDown("s") then
-        y = y + 1
-    end
-
-    if love.keyboard.isDown("up") or love.keyboard.isDown("w") then
-        y = y - 1
-    end
-
-    if player.anim == player.animations.attack and player.anim.position == 4 then
-        player.anim = player.animations.idle
-    end
-
-    if love.keyboard.isDown("space") then
-        player.anim = player.animations.attack
-        table.insert(actors.foreground, deepcopy(enemy))
-        CreateTimer("atk", _FL * 4 - 0.01)
-    end
-
-    player.x = player.x + x * player.speed
-    player.y = player.y + y * player.speed
-
-    if x < 0 then
-        player.looking_right = true
-    elseif x > 0 then
-        player.looking_right = false
-    end
-
-    player.anim.flippedH = player.looking_right
-
-    player.anim:update(dt)
+    UpdateAnimations({player, unpack(enmy)}, dt)
     
+    EnemyUpdate()
+    PlayerUpdate()
 end
 
 
@@ -114,14 +66,13 @@ function love.draw()
     love.graphics.draw(background.planet, 0, 0, 0, _DS, _DS)
 
     -- behind enemies
-
+    -- DrawEntities({})
 
     -- arena
     love.graphics.draw(background.arena, 0, 0, 0, _DS, _DS)
 
     -- foreground
-    DrawEntities(actors.foreground)
-    DrawEntities({player})
+    DrawEntities({player, unpack(enmy)})
 end
 
 
@@ -135,6 +86,7 @@ function TickTimers(dt)
     for k, v in pairs(timers) do
         timers[k] = timers[k] - dt
         if timers[k] <= 0 then
+            --print(k)
             table.removekey(timers, k)
         end
     end
@@ -169,6 +121,7 @@ end
 function DrawEntities(entities)
     EntityInsertionSort(entities)
     for i = 1, len(entities) do
+        entities[i].anim.flippedH = not entities[i].looking_right
         entities[i].anim:draw(spriteSheet, entities[i].x, entities[i].y, nil, _DS, _DS)
     end
 end
@@ -188,8 +141,100 @@ function deepcopy(orig)
             copy[deepcopy(orig_key)] = deepcopy(orig_value)
         end
         setmetatable(copy, deepcopy(getmetatable(orig)))
-    else -- number, string, boolean, etc
+    else
         copy = orig
     end
     return copy
+end
+
+function SpawnEnemy()
+    e = deepcopy(enemy)
+    e.x = math.random(100, 550)
+    e.y = math.random(170, 280)
+
+    last_id = last_id + 1
+    e.id = last_id 
+    CreateTimer("e" .. tostring(e.id), _FL * 4 - 0.01)
+
+    e.anim = e.animations.spawn
+
+    if e.x > player.x then
+        e.looking_right = false
+    end
+    
+    table.insert(enmy, e)
+end
+
+function EnemyUpdate()
+    for i = 1, len(enmy)do
+        e = enmy[i]
+
+        if timers["e" .. e.id] ~= nil then goto continue end
+
+        if math.abs(e.x - player.x) <= e.attack_distance * _DS and math.abs(e.y - player.y) <= e.attack_distance * _DS / 2 then
+            e.anim = e.animations.attack
+            CreateTimer("e" .. e.id, 4 * _FL -0.01)
+            goto continue
+        end
+        
+        if e.x < player.x then
+            e.looking_right = true
+            e.x = e.x + e.speed
+        else
+            e.looking_right = false
+            e.x = e.x - e.speed
+        end
+
+        if e.y < player.y then
+            e.y = e.y + e.speed
+        else
+            e.y = e.y - e.speed
+        end
+
+        e.anim = e.animations.idle
+
+        ::continue::
+    end
+end
+
+function PlayerUpdate()
+    if timers["atk"] ~= nil then
+        return
+    end
+
+    x = 0
+    y = 0
+
+    if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
+        x = x + 1
+    end
+
+    if love.keyboard.isDown("left") or love.keyboard.isDown("a") then
+        x = x - 1
+    end
+
+    if love.keyboard.isDown("down") or love.keyboard.isDown("s") then
+        y = y + 1
+    end
+
+    if love.keyboard.isDown("up") or love.keyboard.isDown("w") then
+        y = y - 1
+    end
+
+    player.anim = player.animations.idle
+
+    if love.keyboard.isDown("space") then
+        player.anim = player.animations.attack
+        SpawnEnemy()
+        CreateTimer("atk", _FL * 4 - 0.01)
+    end
+
+    player.x = player.x + x * player.speed
+    player.y = player.y + y * player.speed
+
+    if x > 0 then
+        player.looking_right = true
+    elseif x < 0 then
+        player.looking_right = false
+    end
 end
